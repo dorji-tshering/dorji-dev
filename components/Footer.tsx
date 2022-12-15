797c9c9303132f34d7f908a8b6ed94d892ab8189
Jigthril Kinley Yoesel <dorjitshering370@gmail.com>
2022-12-15 21:19:04 +0600

    Updated footer links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,10 @@ const Footer = () => {
     return (
         <div className="py-6 flex justify-center items-center flex-col text-xs text-gray-600
             border-t border-dashed border-gray-700 font-[500]">
-            <p className="mb-1">Built with <a className="text-theme font-bold" href="https://nextjs.org" target="_blank">NextJs</a></p>
+            <p className="mb-1">Built with <a className="text-[#0070f3] font-[500]" href="https://nextjs.org" target="_blank">NextJs</a>
+                <span className="mx-1">+</span>
+                <a href="https://tailwindcss.com/" target="_blank" className='font-[500] text-[#0ea5e9]'>Tailwind</a>
+            </p>
             <div className='flex flex-col items-center sm:flex-row'>
                 <p className='flex items-center flex-wrap'>Copyright 
                     <AiOutlineCopyright size={12} className='inline mx-1'/>
