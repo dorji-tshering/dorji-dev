fdcefac19110aa48272836dfd562c7bff9f4717b
Dorji Tshering <dorjitshering370@gmail.com>
2022-12-17 00:05:23 +0600

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -32,7 +32,7 @@ const about = () => {
                         </p>
                         <p className="mb-6">
                             I have built a fully featured e-commerce website using Wordpress/Woocommerce for a client which you can 
-                            visit <Link className="text-gray-400 underline underline-offset-2 decoration-dotted hover:text-theme transition-all duration-500" target="_blank" href="https://tuo.shopping">here</Link> . 
+                            visit <Link className="text-gray-400 underline underline-offset-2 decoration-dotted hover:text-theme transition-all duration-500" target="_blank" href="https://tuo.shopping">here</Link>. 
                             Now I am fully confident that I can build any 
                             production-ready website with Wordpress to the customization of the client.
                         </p>
